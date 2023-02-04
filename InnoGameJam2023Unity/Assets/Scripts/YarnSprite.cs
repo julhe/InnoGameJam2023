9b3e46f336a466f0c5d5275e4eb27e6d30366557
Julian Heinken <hi@julianheinken.com>
2023-02-04 20:30:02 +0100

    Added granny animation

---
+++ InnoGameJam2023Unity/Assets/Scripts/YarnSprite.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using Spine.Unity;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+using Yarn.Unity;
+
+public class YarnSprite : MonoBehaviour
+{
+    [YarnCommand("ShowCharacter")]
+    public static void ShowCharacter(String refName, bool state) {
+        foreach (YarnSprite yarnSprite in FindObjectsOfType<YarnSprite>()) {
+            if (yarnSprite.name != refName) continue;
+            yarnSprite.SetState(state);
+            break;
+        }
+        
+        
+    }
+
+    void OnEnable() {
+        GetComponent<MaskableGraphic>().color = Color.white.WithAlpha(0.0f);
+    }
+
+    void SetState(bool state) {
+        GetComponent<MaskableGraphic>().DOFade(state ? 1.0f : 0.0f, 2.0f);
+    }
+}
