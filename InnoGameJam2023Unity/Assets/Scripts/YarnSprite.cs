43d7d8998db09a2197f8032d1e8c22df5b005599
Julian Heinken <hi@julianheinken.com>
2023-02-05 00:08:21 +0100

    New Leonie animation, finsihed mert writing

--- InnoGameJam2023Unity/Assets/Scripts/YarnSprite.cs
+++ InnoGameJam2023Unity/Assets/Scripts/YarnSprite.cs
@@ -17,15 +17,32 @@ public class YarnSprite : MonoBehaviour
             yarnSprite.SetState(state);
             break;
         }
-        
-        
+    }
+    [YarnCommand("SetAnimation")]
+    public static void SetAnimation(String refName, String animationName, int trackIndex, bool play) {
+        foreach (YarnSprite yarnSprite in FindObjectsOfType<YarnSprite>()) {
+            if (yarnSprite.name != refName) continue;
+
+            if (!yarnSprite.TryGetComponent(out SkeletonGraphic skeletonGraphic)) continue;
+            
+            if (play) {
+                skeletonGraphic.AnimationState.AddAnimation(trackIndex, animationName, true, 0.0f);
+            }
+            else {
+                skeletonGraphic.AnimationState.ClearTrack(trackIndex);
+            }
+        }
     }
 
     void Start() {
         GetComponent<MaskableGraphic>().color = Color.white.WithAlpha(0.0f);
+
+
     }
 
     void SetState(bool state) {
         GetComponent<MaskableGraphic>().DOFade(state ? 1.0f : 0.0f, 2.0f);
     }
+    
+
 }
